1eddb691fb7fac7b6b8be1013f23efc9e8a2d366
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-09-01 17:01:01 +0530

    auth

--- app.js
+++ app.js
@@ -45,12 +45,25 @@ app.use((req, res, next) => {
     'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if(req.method === "OPTIONS"){
+    return res.sendStatus(200);
+  }
   next();
 });
 
 app.use('/graphql', graphqlHttp({
   schema: graphqlSchema,
-  rootValue: graphqlResolver
+  rootValue: graphqlResolver,
+  graphiql:true,
+  formatError(err){
+    if(!err.originalError){
+      return err;
+    }
+    const data = err.originalError.data;
+    const message = err.message || "An error occured.";
+    const code = err.originalError.code || 500;
+    return {message: message, status: code, data: data}
+  } 
 }));
 
 app.use((error, req, res, next) => {
@@ -67,6 +80,7 @@ mongoose
   )
   .then(result => {
     app.listen(8080);
+    
     
   })
   .catch(err => console.log(err));
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,65 @@
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
 module.exports = {
-    hello(){
-        return {
-            text: "Hello World", 
-            views: 1234
+    createUser: async function({ userInput }, req){
+        // const email = args.UserInput.email
+        const errors = [];
+        if(!validator.isEmail(userInput.email)){
+            errors.push({message: "Email is not valid"})
         }
+        if(validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, { min: 5 } )){
+            errors.push({message: "Password too short!!"})
+        }
+
+        if(errors.length > 0){
+            const error = new Error('Invalid input.');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
+        const existingUser = await User.findOne({email: userInput.email});
+        if(existingUser){
+            const error = new Error("User exists already!");
+            throw error;
+        }
+
+        const hashedPw = await bcrypt.hash(userInput.password, 12);
+        const user  = new User({
+            email: userInput.email, 
+            name: userInput.name,
+            password: hashedPw
+        });
+
+        const createdUser = await user.save();
+
+        return { ...createdUser._doc, _id:createdUser._id.toString() };
+
+    },
+    login: async function ({email, password}){
+        const user = await User.findOne({email: email});
+        if(!user){
+            const error = new Error('User not found.');
+            error.code = 401;
+            throw error;
+        }
+        const isEqual = bcrypt.compare(password, user.password);
+        if(!isEqual){
+            const error = new Error('Password is incorrect.');
+            error.code = 401;
+            throw error;
+        }
+
+        const token = jwt.sign({
+            userId : user._id.toString(),
+            email: user.email
+        }, 'thisismysecret', { expiresIn: '1h'});
+
+        return {token: token, userId: user._id.toString() };
+
     }
+
+
 }
