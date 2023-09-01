7c8f0f8144cc542f3cd925a8f9b9538b685b974c
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-09-01 18:21:37 +0530

    create post

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const  graphqlHttp  = require('express-graphql').graphqlHTTP;
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
 
+const auth = require('./middleware/auth');
 const app = express();
 
 const fileStorage = multer.diskStorage({
@@ -50,6 +51,8 @@ app.use((req, res, next) => {
   }
   next();
 });
+
+app.use(auth);
 
 app.use('/graphql', graphqlHttp({
   schema: graphqlSchema,
