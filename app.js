49969c19860f960a5998f94895f1a19f3ca30454
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-09-03 11:59:42 +0530

    create pagination

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -54,6 +55,22 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+  if(!req.isAuth){
+    throw new Error("Unauthenticated!")
+  }
+  if(!req.file){
+    return res.status(200).json({message: "No file provided"});
+  }
+  if(req.body.oldPath){
+    clearImage(req.body.oldPath);
+  }
+
+  return res.status(201).json({ message: "File stored.", filePath: req.file.path })
+
+})
+
+
 app.use('/graphql', graphqlHttp({
   schema: graphqlSchema,
   rootValue: graphqlResolver,
@@ -87,3 +104,8 @@ mongoose
     
   })
   .catch(err => console.log(err));
+
+  const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
+  };
