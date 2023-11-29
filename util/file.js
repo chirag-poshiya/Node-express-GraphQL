868848f92cb36de2efdf9007a3a409790f7eff58
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-11-29 15:55:03 +0530

    t

--- util/file.js
+++ util/file.js
@@ -1,9 +1,9 @@
-// const fs = require('fs');
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-// const clearImage = filePath => {
-//     filePath = path.join(__dirname, '..', filePath);
-//     fs.unlink(filePath, err => console.log(err));
-// };
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
+};
 
-// exports.clearImage = clearImage;
+exports.clearImage = clearImage;
