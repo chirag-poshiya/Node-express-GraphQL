1bb3dc827e727889c1c269b9a5245317cbebbe1b
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-12-07 09:46:37 +0530

    add

--- util/file.js
+++ util/file.js
@@ -2,7 +2,7 @@ const fs = require('fs'); //File system
 const path = require('path'); // path
 
 const clearImage = filePath => {
-    filePath = path.join(__dirname, '..', filePath); 
+    filePath = path.join(__dirname, '..', filePath); // Define path for file
     fs.unlink(filePath, err => console.log(err));
 };
 
