60c32316854b3f912a35015baef9d1f3e455dc0a
chiragpatel3669 <chiragpatel3669@gmail.com>
2023-12-18 18:51:13 +0530

    add

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,13 +2,14 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const authHeader = req.get('Authorization'); //get authorization key from header
-  
+  // Check authHeader
   if (!authHeader) {
     req.isAuth = false;
     return next();
   }
   const token = authHeader.split(' ')[1]; //splitting authorization value from bearer
   let decodedToken;
+  // ex handling 
   try {
     decodedToken = jwt.verify(token, 'thisismysecret');
   } catch (err) {
